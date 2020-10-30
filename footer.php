007798ecec915595ae35d6ba51d77618ccda864f
MartiRabella <73338540+MartiRabella@users.noreply.github.com>
2020-10-30 09:13:14 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="mastfoot mt-auto">
   <div class="inner">
-    <p>Echo por <strong>Marti Rabella</strong>  y <strong>Carlos Erencia</strong>.</p>
+    <p>Hecho por <strong>Marti Rabella</strong>  y <strong>Carlos Erencia</strong>.</p>
   </div>
 </footer>
