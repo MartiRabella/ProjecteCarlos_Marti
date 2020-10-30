bd34509c690cc440d0864ba9801f5c9a041d76d1
usuario <usuario@debian.debian>
2020-10-30 12:06:26 +0100

    update

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@ if (isset($_COOKIE['hechopedido'])) {
   header('Location: error.php');
 }
 
- ?>
+?>
 
 <!doctype html>
 <html lang="es">
@@ -12,6 +12,7 @@ if (isset($_COOKIE['hechopedido'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 		<script type="text/javascript" src="js.js"></script>
 
     <link rel="shortcut icon" type="image/x-icon" href="icono.ico" />
