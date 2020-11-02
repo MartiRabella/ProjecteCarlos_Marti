6e020200388e2cc217504b975ed63de63f0c7966
usuario <usuario@debian.debian>
2020-11-02 12:02:16 +0100

    css

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
  //BASE DE DATOS
-var productos = ["Antivirus", "Grafica", "Disco duro", "Ordenador", "Bolso portatil", "Portatil", "Memoria RAM", "Router Linux", "Sintonizadora TV"];
+var productos = ["Bocadillo Jamon", "Bocadillo Queso", "Bocadillo Chorizo", "Bricoli con patatas", "Macarrones Boloñesa", "Patatas con Huevos", "Flan de Huevo", "Arroz con Leche", "Fruta de Temporada"];
 var imgGrandes = ["img/productos/1.jpg", "img/productos/2.jpg", "img/productos/3.jpg", "img/productos/4.jpg", "img/productos/5.jpg", "img/productos/6.jpg", "img/productos/7.jpg", "img/productos/8.jpg", "img/productos/9.jpg"];
 var imgPeque = ["img/productos/1m.jpg", "img/productos/2m.jpg", "img/productos/3m.jpg", "img/productos/4m.jpg", "img/productos/5m.jpg", "img/productos/6m.jpg", "img/productos/7m.jpg", "img/productos/8m.jpg", "img/productos/9m.jpg"];
-var precios = [33, 169, 36, 360, 11, 540, 21, 66, 25];
-var stock = [5, 2, 8, 3, 10, 4, 3, 1, 2];
-var precioTransporte = [6, 12, 20, "gratis"];
-var IVA = 0.18;
+var precios = [3, 2, 2, 5, 4, 5, 3, 2, 1];
+var stock = [5, 5, 5, 5, 5, 5, 5, 5, 5];
+var precioTransporte = ["gratis", "gratis", "gratis", "gratis"];
+var IVA = 0.10;
 var uniUser;
 
 //JAVASCRIPT A EJECUTARSE UNA VEZ CARGADA LA PAGINA: 
@@ -22,8 +22,6 @@ document.getElementById("botonTotal").onclick = validaLasUnidades;
 document.getElementById("botonDatos").onclick = pideDatos;
 document.getElementById("botonConfirmar").onclick = validaDatosPersonales;
 }
-
-/*-------------------COMIENZAN LAS FUNCIONES-------------------*/
 
 //FUNCION DE VALIDACION DE UNIDADES:
 function validaLasUnidades(elEvento) {
